0444762977a7e8f929d33611c2c37eb219bbce0d
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-12 08:07:19 +0300

    Add properties for Renter Model

--- ResourceControlingAPI/ResourceControlingAPI/Models/Renters.cs
+++ ResourceControlingAPI/ResourceControlingAPI/Models/Renters.cs
@@ -1,6 +1,29 @@
-﻿namespace ResourceControlingAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceControlingAPI.Models
 {
     public class Renters
     {
+        [Key]
+        [Required]
+        public int RenterID { get; set; }
+
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public bool IsSubscribed { get; set; }
     }
 }

d5fe987fd4983432c22a7ea00e3e1090d8276aa6
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-12 21:57:09 +0300

    Create MeterDto

---
+++ ResourceControlingAPI/ResourceControlingAPI/Dtos/MeterDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceControlingAPI.Dtos
+{
+    public class MeterDto
+    {
+        [Key]
+        public int MeterId { get; set; }
+        [Required]
+        public int Number { get; set; }
+
+        [Required]
+        public string? Purpose { get; set; }
+
+        [Range(0, double.PositiveInfinity)]
+        public double MaximumAvailableValue { get; set; }
+    }
+}

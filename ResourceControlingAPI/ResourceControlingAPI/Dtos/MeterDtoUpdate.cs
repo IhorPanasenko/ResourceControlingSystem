7cb5990bddb96ae826828cb0cafe69463452f712
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-12 21:57:20 +0300

    CreateMeterDtoUpdate

---
+++ ResourceControlingAPI/ResourceControlingAPI/Dtos/MeterDtoUpdate.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceControlingAPI.Dtos
+{
+    public class MeterDtoUpdate
+    {
+        public int Number { get; set; }
+        public string? Purpose { get; set; }
+
+        [Range(0, double.PositiveInfinity)]
+        public double MaximumAvailableValue { get; set; }
+    }
+}

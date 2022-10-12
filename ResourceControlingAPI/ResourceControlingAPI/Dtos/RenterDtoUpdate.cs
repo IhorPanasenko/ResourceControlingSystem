0c23d668ba0a9162ce26a04600a59b58c654c87d
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-12 13:22:58 +0300

    Create RenterDtoUpdate

---
+++ ResourceControlingAPI/ResourceControlingAPI/Dtos/RenterDtoUpdate.cs
@@ -0,0 +1,19 @@
+﻿namespace ResourceControlingAPI.Dtos
+{
+    public class RenterDtoUpdate
+    {
+        public string? Login { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? SecondName { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? EmailAddress { get; set; }
+
+        public bool IsSubscribed { get; set; }
+    }
+}

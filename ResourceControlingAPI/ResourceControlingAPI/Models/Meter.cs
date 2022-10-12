29d56f6f75121bd24703014bf53a1c2f57b175ef
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-12 21:45:18 +0300

    Add meter model

---
+++ ResourceControlingAPI/ResourceControlingAPI/Models/Meter.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ResourceControlingAPI.Models
+{
+    public class Meter
+    {
+        [Key]
+        public int MeterId { get; set; }
+        [Required]
+        public int Number { get; set; }
+
+        [Required]
+        public string? Purpose { get; set; }
+
+        [Range(0,double.PositiveInfinity)]
+        public double MaximumAvailableValue { get; set; }
+
+    }
+}

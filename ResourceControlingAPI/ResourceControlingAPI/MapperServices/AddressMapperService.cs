124884b7c00a259da6b731c789414a18425b0176
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-12 17:59:14 +0300

    Add AddressMapperService

---
+++ ResourceControlingAPI/ResourceControlingAPI/MapperServices/AddressMapperService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using ResourceControlingAPI.Dtos;
+using ResourceControlingAPI.Models;
+
+namespace ResourceControlingAPI.MapperServices
+{
+    public class AddressMapperService : IMapperService<AddressDto, Address>
+    {
+        private readonly IMapper _mapper;
+        public AddressMapperService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public AddressDto AsDto(Address model)
+        {
+           return _mapper.Map<AddressDto>(model);
+        }
+
+        public List<AddressDto> AsDtoList(List<Address> modelsList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Address AsModel(AddressDto dto)
+        {
+            return _mapper.Map<Address>(dto);
+        }
+
+        public List<Address> AsModelList(List<AddressDto> dtosList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

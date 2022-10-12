ea3cf53e6dbff1d3eb3b10064a065e0ad7b1dc03
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-12 21:57:41 +0300

    Create MeterMapperService

---
+++ ResourceControlingAPI/ResourceControlingAPI/MapperServices/MeterMapperService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using ResourceControlingAPI.Dtos;
+using ResourceControlingAPI.Models;
+
+namespace ResourceControlingAPI.MapperServices
+{
+    public class MeterMapperService : IMapperService<MeterDto, Meter>
+    {
+        private readonly IMapper _mapper;
+
+        public MeterMapperService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        public MeterDto AsDto(Meter model)
+        {
+            return _mapper.Map<MeterDto>(model);
+        }
+
+        public List<MeterDto> AsDtoList(List<Meter> modelsList)
+        {
+            List<MeterDto> Dtos = new List<MeterDto>();
+            foreach (Meter model in modelsList)
+            {
+                Dtos.Add(AsDto(model));
+            }
+            return Dtos;
+        }
+
+        public Meter AsModel(MeterDto dto)
+        {
+            return _mapper.Map<Meter>(dto);
+        }
+
+        public List<Meter> AsModelList(List<MeterDto> dtosList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

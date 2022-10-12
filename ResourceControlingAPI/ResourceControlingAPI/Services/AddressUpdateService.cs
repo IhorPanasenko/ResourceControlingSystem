70d3ada703732a7a22dbb3c2275db0cef49302b2
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-12 18:43:05 +0300

    create AddressUpdateService

---
+++ ResourceControlingAPI/ResourceControlingAPI/Services/AddressUpdateService.cs
@@ -0,0 +1,28 @@
+﻿using ResourceControlingAPI.Dtos;
+using ResourceControlingAPI.Models;
+
+namespace ResourceControlingAPI.Services
+{
+    public class AddressUpdateService : IUpdateService<Address, AddressDtoUpdate>
+    {
+        public void Update(Address model, AddressDtoUpdate dtoUpdate)
+        {
+            if(dtoUpdate.City != null)
+            {
+                model.City = dtoUpdate.City; 
+            }
+            if(dtoUpdate.StreetName != null)
+            {
+                model.StreetName = dtoUpdate.StreetName;
+            }
+            if(dtoUpdate.HouseNumber > 0)
+            {
+                model.HouseNumber = dtoUpdate.HouseNumber;
+            }
+            if(dtoUpdate.FlatNumber > 0)
+            {
+                model.FlatNumber = dtoUpdate.FlatNumber;
+            }
+        }
+    }
+}

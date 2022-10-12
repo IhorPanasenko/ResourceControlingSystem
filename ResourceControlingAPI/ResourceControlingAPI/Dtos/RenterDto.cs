5fc4863c4a77ef016ed81c8b6514eb47f1db5828
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-12 11:35:45 +0300

    Create RenterDto with properties

---
+++ ResourceControlingAPI/ResourceControlingAPI/Dtos/RenterDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceControlingAPI.Dtos
+{
+    public class RenterDto
+    {
+        [Key]
+        public int RenterID { get; set; }
+
+        public string? Login { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        public string? SecondName { get; set; }
+
+        [Required]
+        public string? PhoneNumber { get; set; }
+
+        public string? EmailAddress { get; set; }
+
+        public bool IsSubscribed { get; set; }
+    }
+}

6764bbf116a9d7407579d22e21ec8f37e73544b9
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-12 14:44:57 +0300

    Create Address Model

---
+++ ResourceControlingAPI/ResourceControlingAPI/Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceControlingAPI.Models
+{
+    public class Address
+    {
+        [Key]
+        public int AddressId { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string StreetName { get; set; }
+
+        [Required]
+        public int HouseNumber { get; set; }
+
+        [Required]
+        public int FlatNumber { get; set; }
+
+
+    }
+}

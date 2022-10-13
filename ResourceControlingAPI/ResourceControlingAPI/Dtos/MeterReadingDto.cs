cc128192c0cf5b1281fdff9a1b99cc204c1af579
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-13 09:55:31 +0300

    Create MeterReadingDto

---
+++ ResourceControlingAPI/ResourceControlingAPI/Dtos/MeterReadingDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResourceControlingAPI.Dtos
+{
+    public class MeterReadingDto
+    {
+        [Key]
+        public int MeterReadingId { get; set; }
+
+        [Required]
+        public int ReadingNumbers { get; set; }
+
+        [Required]
+        public DateTime DateTimeReading { get; set; }
+
+        public int MeterId { get; set; }
+
+    }
+}

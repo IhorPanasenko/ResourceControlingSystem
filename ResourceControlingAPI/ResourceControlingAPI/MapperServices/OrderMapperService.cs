fd654e0d572c7d3c2718dfca7d7b6849b25de147
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-13 12:22:23 +0300

    Create OrderMapperService

---
+++ ResourceControlingAPI/ResourceControlingAPI/MapperServices/OrderMapperService.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using ResourceControlingAPI.Dtos;
+using ResourceControlingAPI.Models;
+
+namespace ResourceControlingAPI.MapperServices
+{
+    public class OrderMapperService : IMapperService<OrderDto, Order>
+    {
+        private readonly IMapper _mapper;
+
+        public OrderMapperService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        public OrderDto AsDto(Order model)
+        {
+            return _mapper.Map<OrderDto>(model);
+        }
+
+        public List<OrderDto> AsDtoList(List<Order> modelsList)
+        {
+            List<OrderDto> result = new List<OrderDto>();
+
+            foreach (Order model in modelsList)
+            {
+                result.Add(AsDto(model));
+            }
+
+            return result;
+        }
+
+        public Order AsModel(OrderDto dto)
+        {
+            return _mapper.Map<Order>(dto);
+        }
+
+        public List<Order> AsModelList(List<OrderDto> dtosList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

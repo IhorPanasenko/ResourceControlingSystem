6f72700da432ba6533b682b7e4b86c4c12592d01
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-13 06:41:50 +0300

    Create Order Model

---
+++ ResourceControlingAPI/ResourceControlingAPI/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResourceControlingAPI.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int NumberOfDevices { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string POstalOficeName { get; set; }
+
+        [Required]
+        public int PostalOficeNumber { get; set; }
+
+        [Required]
+        public DateTime DateOfOrder { get; set; }
+
+        [ForeignKey("RenterId")]
+        public int RenterId { get; set; }
+        public Renter Renter { get; set; }
+    }
+}

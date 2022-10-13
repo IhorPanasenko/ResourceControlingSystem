9e355e2079c561de5ec2891009907e8bccd174b3
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-13 10:01:09 +0300

    Rename class

--- ResourceControlingAPI/ResourceControlingAPI/Dtos/MeterReadingDtoUpdate.cs
+++ ResourceControlingAPI/ResourceControlingAPI/Dtos/MeterReadingDtoUpdate.cs
@@ -1,10 +1,10 @@
 ﻿namespace ResourceControlingAPI.Dtos
 {
-    public class MeterReadingUpdateDto
+    public class MeterReadingDtoUpdate
     {
         public int ReadingNumbers { get; set; }
 
-        public DateTime DateTimeReading { get; set; }
+        public DateTime? DateTimeReading { get; set; }
 
         public int MeterId { get; set; }
     }

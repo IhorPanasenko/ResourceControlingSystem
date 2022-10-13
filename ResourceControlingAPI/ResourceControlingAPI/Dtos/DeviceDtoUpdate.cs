500ccb68c52d4272c810009372dabc1e8de53301
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-13 14:50:35 +0300

    Add DeviceDtos

---
+++ ResourceControlingAPI/ResourceControlingAPI/Dtos/DeviceDtoUpdate.cs
@@ -0,0 +1,11 @@
+﻿using ResourceControlingAPI.Models;
+
+namespace ResourceControlingAPI.Dtos
+{
+    public class DeviceDtoUpdate
+    {
+        public int DeviceId { get; set; }
+        public int AddressId { get; set; }
+        public int MeterId { get; set; }
+    }
+}

0387ef0f77b0d09dfc39ffc1251b6d8824d74683
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-13 07:42:01 +0300

    Add Warehouse Dtos

---
+++ ResourceControlingAPI/ResourceControlingAPI/Dtos/WarehouseDtoUpdate.cs
@@ -0,0 +1,11 @@
+﻿namespace ResourceControlingAPI.Dtos
+{
+    public class WarehouseDtoUpdate
+    {
+        public int WarehouseId { get; set; }
+
+        public int DevicePrice { get; set; }
+
+        public int AvailableDevices { get; set; }
+    }
+}

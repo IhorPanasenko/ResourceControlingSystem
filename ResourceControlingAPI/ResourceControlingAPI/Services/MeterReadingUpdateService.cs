671f2035a433bfe8dfa869d51f204ad80a2b1798
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-13 10:01:37 +0300

    Create MeterReadingUpdateService

---
+++ ResourceControlingAPI/ResourceControlingAPI/Services/MeterReadingUpdateService.cs
@@ -0,0 +1,24 @@
+﻿using ResourceControlingAPI.Dtos;
+using ResourceControlingAPI.Models;
+
+namespace ResourceControlingAPI.Services
+{
+    public class MeterReadingUpdateService : IUpdateService<MeterReading, MeterReadingDtoUpdate>
+    {
+        public void Update(MeterReading model, MeterReadingDtoUpdate dtoUpdate)
+        {
+            if (dtoUpdate.DateTimeReading != null)
+            {
+                model.DateTimeReading = dtoUpdate.DateTimeReading;
+            }
+            if(dtoUpdate.ReadingNumbers > 0)
+            {
+                model.ReadingNumbers = dtoUpdate.ReadingNumbers;
+            }
+            if(dtoUpdate.MeterId > 0)
+            {
+                model.MeterId = dtoUpdate.MeterId;
+            }
+        }
+    }
+}

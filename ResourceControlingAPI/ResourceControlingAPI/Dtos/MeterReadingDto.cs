d82137d1bdb60408721a6b2b8df3411311573b21
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-13 10:33:13 +0300

    Update MeterReadingDto

--- ResourceControlingAPI/ResourceControlingAPI/Dtos/MeterReadingDto.cs
+++ ResourceControlingAPI/ResourceControlingAPI/Dtos/MeterReadingDto.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ResourceControlingAPI.Models;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ResourceControlingAPI.Dtos
 {
@@ -15,6 +17,8 @@ namespace ResourceControlingAPI.Dtos
         public DateTime? DateTimeReading { get; set; }
 
         public int MeterId { get; set; }
+
+        public MeterDto? Meter { get; set; }
 
     }
 }

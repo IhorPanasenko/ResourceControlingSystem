e853898d05afa34f0a14a7713a51a66bc09ade16
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-14 18:58:48 +0300

    Create AdminDto

---
+++ ResourceControlingAPI/ResourceControlingAPI/Dtos/AdminDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceControlingAPI.Dtos
+{
+    public class AdminDto
+    {
+        [Key]
+        public int AdminId { get; set; }
+
+        [Required]
+        public string? Login { get; set; }
+        
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public string FirstName { get; set; } = "admin";
+
+        [Required]
+        public string SecondName { get; set; } = "admin";
+
+        [Required]
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+    }
+}
---
+++ ResourceControlingAPI/ResourceControlingAPI/Dtos/AdminDtoUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceControlingAPI.Dtos
+{
+    public class AdminDtoUpdate
+    {
+        public string? Login { get; set; }
+
+        public string? Password { get; set; }
+
+        public string FirstName { get; set; } = "admin";
+
+        public string SecondName { get; set; } = "admin";
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+    }
+}

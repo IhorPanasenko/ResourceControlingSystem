1dccb4172fab137bda35150555bf328ddaa16096
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-17 19:58:40 +0300

    Add authorization for admin

--- ResourceControlingAPI/ResourceControlingAPI/Controllers/AdminController.cs
+++ ResourceControlingAPI/ResourceControlingAPI/Controllers/AdminController.cs
@@ -1,11 +1,19 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using ResourceControlingAPI.Data;
 using ResourceControlingAPI.Dtos;
 using ResourceControlingAPI.MapperServices;
+using ResourceControlingAPI.Models;
 using ResourceControlingAPI.Services;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ResourceControlingAPI.Controllers
 {
@@ -14,16 +22,18 @@ namespace ResourceControlingAPI.Controllers
     public class AdminController : ControllerBase
     {
         private readonly ApplicationDbContext _dbContext;
-
+        private readonly IConfiguration _configuration;
         private readonly AdminMapperService _mapperService;
 
-        public AdminController(ApplicationDbContext dbContext, IMapper mapper)
+        public AdminController(ApplicationDbContext dbContext, IMapper mapper, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _configuration = configuration;
             _mapperService = new AdminMapperService(mapper);
         }
 
         [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
         public async Task<IActionResult> GetAll()
         {
             var admins = await _dbContext.Admins.ToListAsync();
@@ -33,6 +43,7 @@ namespace ResourceControlingAPI.Controllers
 
         [HttpGet]
         [Route("{id=int}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
         public async Task<IActionResult> Get([FromRoute] int id)
         {
             var admin = await _dbContext.Admins.FindAsync(id);
@@ -47,6 +58,7 @@ namespace ResourceControlingAPI.Controllers
         }
 
         [HttpPost]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
         public async Task<IActionResult> Create(AdminDto adminDto)
         {
             if (adminDto == null)
@@ -62,6 +74,7 @@ namespace ResourceControlingAPI.Controllers
 
         [HttpDelete]
         [Route("{id=int}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
             var admin = await _dbContext.Admins.FindAsync(id);
@@ -79,6 +92,7 @@ namespace ResourceControlingAPI.Controllers
 
         [HttpPut]
         [Route("{id=int}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
         public async Task<IActionResult> Update([FromRoute] int id, AdminDtoUpdate dtoUpdate)
         {
             var admin = await _dbContext.Admins.FindAsync(id);
@@ -95,6 +109,51 @@ namespace ResourceControlingAPI.Controllers
             await _dbContext.SaveChangesAsync();
             return Ok(adminDto);
         }
+        [HttpPost("Login")]
+        public IActionResult Login(UserLogin login)
+        {
+            var user = _dbContext.Admins.FirstOrDefault(r => r.Login == login.Login && r.Password == login.Password);
+            var claims = new[] { new Claim(ClaimTypes.NameIdentifier, login.Login) };
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var tokenString = getToken(user);
+
+            return Ok(tokenString);
+
+        }
+
+        private string getToken(Admin user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Login!),
+                new Claim(ClaimTypes.Email, user.EmailAddress!),
+                new Claim(ClaimTypes.GivenName, user.FirstName!),
+                new Claim(ClaimTypes.Surname, user.SecondName!),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(60),
+                notBefore: DateTime.UtcNow,
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])),
+                    SecurityAlgorithms.HmacSha256)
+                );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenString;
+        }
+
+
 
     }
 }
